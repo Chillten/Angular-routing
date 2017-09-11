271a61889b87f4a4d43cd750f367aa8538e31bd2
chillten <bogovich.alexander@gmail.com>
2017-09-11 22:54:25 +0700

    Implement child routes for servers and users

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 import {ServersService} from '../servers.service';
-import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -12,7 +12,8 @@ export class ServerComponent implements OnInit {
   server: { id: number, name: string, status: string };
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -22,6 +23,12 @@ export class ServerComponent implements OnInit {
     } else {
       this.server = this.serversService.getServer(1);
     }
+
+    this.route.params.subscribe((params: Params) =>
+      this.server = this.serversService.getServer(+params['id']))
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  }
 }
