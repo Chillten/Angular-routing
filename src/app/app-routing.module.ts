5027ced5448e9541581ac6d959d373c2c7b7b802
chillten <bogovich.alexander@gmail.com>
2017-09-16 16:31:47 +0700

    Secure Servers childes by canActivateChild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,10 +14,14 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent , children: [
     { path: ':id/:name', component: UserComponent }
   ]},
-  { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
+  { path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
     { path: ':id/edit', component: EditServerComponent },
     { path: ':id', component: ServerComponent }
-  ]},
+    ]},
   { path: 'not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/not-found' }
 ];
